79a8c620bab786decfea3d0b3d910ff297355946
Justin Bergkamp <j.bergkamp2@gmail.com>
2017-02-06 17:50:42 -0500

    Oh Baby

---
+++ Cooper Word Replacement Extension/README.md
@@ -0,0 +1,28 @@
+The Official Cooper Word Replacement Extension:
+
+Google Chrome extension that replaces all instances of some Cooper buzz words with their more fun equivalents. Check the replacements.txt file for the full list. 
+
+INSTRUCTIONS:
+The extension isn't live yet, but will still work by following these steps.
+
+1) Download this folder from Github 
+
+2) Download and/or open Google Chrome
+
+3) Go to chrome://extensions/
+
+4) Check the box for Developer Mode
+
+5) Click the button "Load unpacked extensions"
+
+6) Navigate to this folder, choose it
+
+7) You're Done! Refresh the extensions page then check out the results on FB or any other site. Feel free to add you own by following instructions in the contents.js file. 
+
+
+
+
+Tutorial followed: 
+https://9to5google.com/2015/06/14/how-to-make-a-chrome-extensions/
+
+Code Credit to Tom Maxwell -  "How-to: Make your own text-replacing Chrome extension like ‘Millennials to Snake People’"
---
+++ Cooper Word Replacement Extension/content.js
@@ -0,0 +1,35 @@
+var elements = document.getElementsByTagName('*');
+
+var replacements = {};
+replacements["oo"] = "∞";
+replacements["Luke Farrell"] = " trash ";
+replacements["Eric"] = "Barack";
+replacements["Lardaro"] = "the Snake";
+
+/*
+ADD YOUR OWN
+pretty self explanitory but you can copy-paste this line
+replacements[""] = "";
+and fill in the blanks. 
+Then, just put it beneath the examples higher on the page
+*/
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            for(key in replacements){
+                if(text.includes(key)){
+                    var rt = text.replace(key,replacements[key]);
+                    if (rt !== text) {
+                        element.replaceChild(document.createTextNode(rt), node);
+                    }
+                }
+            }
+        }
+    }
+}
